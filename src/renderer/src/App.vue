4a9ef0349d6197fc8b3b2a83195b12198e464aa8
CC <kitten-c@qq.com>
2024-11-19 16:06:44 +0800

    添加开始结束音频视频计时器

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,11 +1,21 @@
 <script setup>
 import { onMounted, ref } from 'vue'
+import Timer from '../utils/timer'
 
 const MokeExternalParameters = {
-  id: 111111,
+  id: 211012,
+  time: 60 * 10000
 }
 
-const externalParameters = ref({})
+const videoSrc = ref()
+const audioSrc = ref()
+const videoRef = ref()
+const audioRef = ref()
+let timer
+const showTimeText = ref(0)
+
+const externalParameters = ref({ ...MokeExternalParameters })
+
 window.electron.onSetFolderPath((params) => {
   console.log('params', params)
   if (import.meta.env.MODE === 'development') {
@@ -15,18 +25,65 @@ window.electron.onSetFolderPath((params) => {
   }
 })
 
+const deviceC = {
+  start() {
+    console.log('deviceC.start')
+
+    videoRef.value.play()
+    audioRef.value.play()
+    timer.start()
+    window.dll({ name: 'treadmillDll', function: 'StartTreadmill', data: [1, 3] })
+  },
+  stop() {
+    videoRef.value.pause()
+    audioRef.value.pause()
+    timer.pause()
+    console.log('videoRef.value?.currentTime', videoRef.value?.currentTime)
+
+    window.dll({ name: 'treadmillDll', function: 'StopTreadmill', data: [3] })
+  },
+  addVal() {
+    window.dll({ name: 'treadmillDll', function: 'AddVel', data: [100, 3] })
+  },
+  delVel() {
+    window.dll({ name: 'treadmillDll', function: 'DelVel', data: [100, 3] })
+  }
+}
+
+const handleTime = (time, untime) => {
+  showTimeText.value = untime /1000
+  console.log('time', untime, videoRef.value?.currentTime)
+}
+
 onMounted(() => {
+  const up = import.meta.env.MODE === 'development' ? '/@fs' : 'file://'
   window.dll({ name: 'treadmillDll', function: 'InitTreadmill', data: [] })
-
-  // window.dll({ name: 'treadmillDll', function: 'setStartMark', data: [] })
-
-  // window.dll({ name: 'treadmillDll', function: 'setStartMark', data: [] })
+  const id = externalParameters.value.id
+  videoSrc.value = `${up}/D:/assets/video_${id}.mp4`
+  audioSrc.value = `${up}/D:/assets/video_${id}.mp3`
+  timer = new Timer(externalParameters.value.time, 100, handleTime)
 })
 </script>
 
 <template>
-  <div>
-    app
+  <div class="main">
+    <video ref="videoRef" :src="videoSrc" controls></video>
+    <audio ref="audioRef" :src="audioSrc" controls loop></audio>
+    <button @click="deviceC.start">开始</button>
+    <button @click="deviceC.stop">暂停</button>
+    <button @click="deviceC.addVal">加速</button>
+    <button @click="deviceC.delVel">减速</button>
+    <div>
+      {{ showTimeText }}
+    </div>
   </div>
-
 </template>
+
+<style lang="scss">
+.main {
+  video {
+    height: 100%;
+    width: 100%;
+  }
+}
+</style>
