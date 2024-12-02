b86ba7e0f8b2cbaa1227c8f46e96f5685dda1c78
CC <cjh@x.com>
2024-12-02 14:29:40 +0800

    暂停过渡完成

--- src/renderer/src/components/PauseTransition.vue
+++ src/renderer/src/components/PauseTransition.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="pause_transition">
-    暂停
+    <img class="image_pop" src="../assets/ui/training_image_pop.png" alt="">
+    <img class="image_pause" src="../assets/ui/training_image_pause.png" alt="">
+    <div class="text">{{ $t('Process_Exit_Tip') }}</div>
   </div>
 </template>
 <script>
@@ -8,7 +10,7 @@ export default {
 
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 .pause_transition {
   position: fixed;
   width: 100%;
@@ -16,6 +18,36 @@ export default {
   top: 0;
   left: 0;
   z-index: 999;
-  color: aqua;
+  background-color: #00000066;
+
+  .image_pop,
+  .image_pause,
+  .text {
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .image_pop {
+    width: 540px;
+    height: 540px;
+    top: 50%;
+    z-index: 1;
+  }
+
+  .image_pause {
+    width: 167px;
+    height: 167px;
+    top: 45%;
+    z-index: 2;
+  }
+
+  .text {
+    color: #fff;
+    font-size: 32px;
+    top: 55%;
+    z-index: 3;
+
+  }
 }
 </style>
